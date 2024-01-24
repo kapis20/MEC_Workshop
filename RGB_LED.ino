ab246ae9f9774e34bfd295a073cb5b6604dbb43e
kapis20 <kacper.filip.sikorski@gmail.com>
2024-01-24 23:45:27 +0000

    RGB LED codes were updated/ commented

--- RGB_LED.ino
+++ RGB_LED.ino
@@ -1,7 +1,25 @@
+// This code defines three pins, namely PIN_RED, PIN_BLUE, and PIN_GREEN, 
+// which correspond to the digital pins 10, 9, and 6, respectively.
+// These pins are set up as output pins in the setup() function. 
+// In the loop() function, a sequence is repeatedly run to create different
+// olours using analogWrite for PWM (Pulse Width Modulation)
+// on each pin to control the intensity of the respective colour channels, i.e., red, green, and blue.
+// it allows running a pttern of various colours 
+
+//Red: (255, 0, 0)
+//Green: (0, 255, 0)
+//Blue: (0, 0, 255)
+//Yellow: (255, 255, 0)
+//Magenta: (255, 0, 255)
+//Cyan: (0, 255, 255)
+
+
+// created by Kacper Sikorski 
+// 10/01/2024
+
 #define PIN_RED 10
 #define PIN_BLUE 9
 #define PIN_GREEN 6
-
 
 
 void setup() {
@@ -27,13 +45,13 @@ void loop() {
 
   delay(1000); // keep the color 1 second
 
-  // colour code Yellow1	#ffff00	rgb(255,255,0)
+  // output blue for 1 second  Blue: (0, 0, 255)
   analogWrite(PIN_RED,   0);
   analogWrite(PIN_GREEN, 0);
   analogWrite(PIN_BLUE,  255);
 
   delay(1000);
-
+// colour code Yellow1	#ffff00	rgb(255,255,0)
   analogWrite(PIN_RED,   255);
   analogWrite(PIN_GREEN, 255);
   analogWrite(PIN_BLUE,  0);
--- RGB_LED_Function.ino
+++ RGB_LED_Function.ino
@@ -1,3 +1,14 @@
+//This code utilises function to robustly 
+// control RGB LED 
+
+// In Arduino programming, functions are blocks of reusable code that perform a specific task. 
+// Functions help organize code, make it more modular, and enhance readability.
+
+
+
+// created by Kacper Sikorski 
+// 19/01/2024
+
 #define PIN_RED 10
 #define PIN_BLUE 9
 #define PIN_GREEN 6
@@ -12,15 +23,27 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Pass integer values representing RGB colours to function setColour
   setColour(255, 0, 0);
   delay(1000);
+  //Pass integer values representing RGB colours to function setColour
   setColour(0,255,0);
   delay(1000);
+  //Pass integer values representing RGB colours to function setColour
   setColour(0,0,255);
   delay(1000);
 }
+// Function declaration:
+// return_type function_name(parameter_type parameter_name) {
+//   // Function body - code to be executed
+//   // ...
+//   return value; // Optional return statement with a value
+// }
 
 void setColour(int RED, int GREEN, int BLUE){
+  //This function takes three integer values representing RGB values
+  //which are passed to analogWrite() to control the RGB LED
+  //No value is returned, therefore void keyword is used 
   analogWrite(PIN_RED,   RED);
   analogWrite(PIN_GREEN, GREEN);
   analogWrite(PIN_BLUE,  BLUE);
